fdb311d7b7e4ff7bcbdfd99022c1ad1adce4b8ae
arzaw <arzaw012@gmail.com>
2022-01-22 14:30:47 +0530

    desktop done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EventComponent } from './event/event.component';
+import { FreeSessionComponent } from './free-session/free-session.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { LearningPathComponent } from './learning-path/learning-path.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'register',
+    component: HeaderComponent
+  },
+  {
+    path: 'free-demo',
+    component: FreeSessionComponent
+  },
+  {
+    path: 'details',
+    component: LearningPathComponent
+  },
+  {
+    path: 'event',
+    component: EventComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
